37bed1ce92122344c2cef1500331a608c594eef8
wdev-jahidhasan <wdev.jahidhasan@gmail.com>
2026-08-24 10:42:24 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
-const express = require('express')
-const app = express()
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
-const PORT = 5000;
+const app = express();
+const PORT = process.env.PORT || 5000;
+const uri = process.env.MONGODB_URI;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } catch (error) {
+    console.error("MongoDB Connection Failed:", error);
+  }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
-  res.send("SERVER IS TOTALLY OKAY")
-})
+  res.send("SERVER IS TOTALLY OKAY");
+});
 
-app.listen( PORT, () => {
-  console.log(`server running on port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
