4bd5471a036b16c156a2fbb5325b62a0a8b72876
wdev-jahidhasan <wdev.jahidhasan@gmail.com>
2026-08-24 09:51:14 +0600

    arthub server created

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+const PORT = 5000;
+
+app.get('/', (req, res) => {
+  res.send("SERVER IS TOTALLY OKAY")
+})
+
+app.listen( PORT, () => {
+  console.log(`server running on port ${PORT}`)
+})
