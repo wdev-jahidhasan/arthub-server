c573e3062465e93d38cb89fd95abacc286097386
wdev-jahidhasan <wdev.jahidhasan@gmail.com>
2026-08-30 19:54:20 +0600

    backend is ready to deploy

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
-    await client.db("admin").command({ ping: 1 });
+    // await client.connect();
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
     const db = client.db("artHub");
